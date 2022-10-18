aa8ff10294a1fdc909a7588da740f560cd0cdc3c
Firat92 <firattezcan92@gmail.com>
2022-10-18 16:55:11 +0300

    first commit

---
+++ src/components/Recipe.js
@@ -0,0 +1,20 @@
+import classes from './recipe.module.css';
+
+const Recipe = ({ title, calories, image, ingredients }) => {
+	return (
+		<div className={classes.recipe}>
+			<h1>{title}</h1>
+			<ul>
+				{ingredients.map((ingredient) => (
+					<li key={Math.random()}>
+						<span>{ingredient.text}</span>
+					</li>
+				))}
+			</ul>
+			<p>{calories.toFixed(2)} cal</p>
+			<img src={image} alt='' className={classes.image} />
+		</div>
+	);
+};
+
+export default Recipe;
